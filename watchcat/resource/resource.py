0f46792cbe839c0dfa9c689804b38cc43f30b8cc
tomy-0000 <yutotom0705@gmail.com>
2021-10-12 00:52:51 +0900

    initの引数を追加

--- watchcat/resource/resource.py
+++ watchcat/resource/resource.py
@@ -1,6 +1,21 @@
+from watchcat.notifier.notifier import Notifier
+
+
 class Resource:
-    def __init__(self, title: str, enabled: bool):
+    def __init__(self, title: str, notifier: Notifier, enabled: bool = True):
+        """init
+
+        Parameters
+        ----------
+        title : str
+            タイトル
+        notifier : Notifier
+            通知元
+        enabled : bool, optional
+            通知するかどうか, by default True
+        """
         self.title = title
+        self.notifier = notifier
         self.enabled = enabled
 
     def get():
