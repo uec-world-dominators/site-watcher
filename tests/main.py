edb2d322b6e264c065592a9412026289398635a0
tomy-0000 <yutotom0705@gmail.com>
2021-10-12 00:12:49 +0900

    test_http_resource関数の追加

--- tests/main.py
+++ tests/main.py
@@ -1,12 +1,20 @@
-import sys
 import os
+import sys
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-import watchcat.__main__
 import unittest
+
+import watchcat.__main__
+from watchcat.resource.http_resource import HttpResource
+
 
 class Test(unittest.TestCase):
     def test_main(self):
         watchcat.__main__.main()
 
-if __name__ == '__main__':
+    def test_http_resource(self):
+        pass
+
+
+if __name__ == "__main__":
     unittest.main()
