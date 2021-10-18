6773fa4bd2a8baf3425531ca4cf9448c19a99798
tomy-0000 <yutotom0705@gmail.com>
2021-10-18 19:02:19 +0900

    Storageクラスの実装完了

--- watchcat/storage/storage.py
+++ watchcat/storage/storage.py
@@ -1,2 +1,18 @@
+from watchcat.snapshot import Snapshot
+
+
 class Storage:
-    pass
+    def __init__(self, db_path: str):
+        """init
+
+        Parameters
+        ----------
+        db_path : str
+            dbの保存場所
+        """
+
+    def get(self, resource_id: str):
+        raise NotImplementedError()
+
+    def set(self, snapshot: Snapshot):
+        raise NotImplementedError()
