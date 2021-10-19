06ce90f1cb859484f439b3a5416eceb997f76abf
shosatojp <me@shosato.jp>
2021-10-19 18:25:26 +0900

    storage対応

--- watchcat/__main__.py
+++ watchcat/__main__.py
@@ -1,6 +1,9 @@
+import sys
 from watchcat import cli
 from watchcat.config.config import ConfigLoader
 from watchcat.diff_detector.simple import SimpleDiffDetector
+from watchcat.storage.storage import Storage
+from watchcat.storage.sql_storage import SqlStorage
 
 
 def main():
@@ -9,11 +12,18 @@ def main():
 
     config = ConfigLoader(args.config)
     diff_detector = SimpleDiffDetector()
+    storage: Storage = SqlStorage("hoge.db")
 
-    for resource in config.resources:
+    for resource_id, resource in config.resources.items():
         new_snapshot = resource.get()
-        # storage.set(resource_id, new_snapshot)
-        # old_snapshot = storage.get(resource_id)
-        if diff_detector.has_update(old_snapshot, new_snapshot):
-            message = f"{resource.title} has updated!"
-            resource.notifier.send(message)
+        old_snapshot = storage.get(resource_id)
+        storage.set(new_snapshot)
+        print(resource)
+        if old_snapshot is not None:
+            if diff_detector.has_update(old_snapshot, new_snapshot):
+                message = f"{resource.title} has updated!"
+                resource.notifier.send(message)
+            else:
+                print(f"no update found for {resource_id}", file=sys.stderr)
+        else:
+            print(f"first fetch for {resource_id}", file=sys.stderr)
